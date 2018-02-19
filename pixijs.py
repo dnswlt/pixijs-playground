4bbb78b5bc4cdd753ab4118284381295490e1869
Dennis Walter <dennis.walter@gmail.com>
2018-02-19 21:12:00 +0100

    Comparison vanilla canvas vs PIXI.js

--- pixijs.py
+++ pixijs.py
@@ -8,5 +8,14 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route('/canvas')
+def canvas():
+    return render_template('canvas.html')
+
+
+@app.route('/pixi')
+def pixi():
+    return render_template('pixi.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
